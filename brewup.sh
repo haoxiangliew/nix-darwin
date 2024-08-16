4fa8ec7e870d0ff66e8123140ec2022f6b1b35bf
Hao Xiang Liew <haoxiangliew@gmail.com>
2024-08-15 22:28:00 -0700

    update lock

--- brewup.sh
+++ brewup.sh
@@ -8,7 +8,7 @@ brew outdated --greedy
 if [ -n "$output" ]; then
 	read -p "These packages will be upgraded, please confirm. [y/N]: " -n 1 -r
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		brew upgrade --greedy
+		brew upgrade --greedy-auto-updates
 	fi
 else
 	echo "No upgradable packages, not doing anything..."
